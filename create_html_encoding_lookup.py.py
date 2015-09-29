c887b312282ceaa75dfd339a4897b29cb50410b7
Mac Gaulin <gaulinmp+git@gmail.com>
2015-09-28 23:17:03 -0500

    Rename html encoding module to be local

--- create_html_encoding_lookup.py.py
+++ create_html_encoding_lookup.py.py
@@ -6,8 +6,8 @@ with open('html_entity_lookups.tsv', 'r') as fh:
     for c in 'string decode unidecode'.split():
         df_lk[c] = df_lk[c].apply(lambda x: x[1:-1] if len(x) > 1 else x)
     df_lk['selected'] = df_lk.apply(lambda x: x.decode if x.lookup == 1 else x.unidecode, axis=1)
-    
-    with open('pyedgar/utilities/html_encoding_lookup.py', 'w') as fh:
+
+    with open('pyedgar/utilities/_html_encoding_lookup.py', 'w') as fh:
         fh.write("""#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -17,7 +17,7 @@ html_ent_re = re.compile('&[^; ]{1,10};')
 
 def html_ent_re_sub_lambda(re_match):
     ent = re_match.group(0)
-    
+
     if ent in HTML_ENCODE_LOOKUP:
         return HTML_ENCODE_LOOKUP[ent]
     return HTML_ENCODE_LOOKUP.get(ent.lower(), ent)
@@ -33,5 +33,5 @@ HTML_ENCODE_LOOKUP = {
             if row.string.lower() not in existing_strings:
                 fh.write('"{0}":{1!r},\n'.format(row.string.lower(), row.selected))
                 existing_strings.append(row.string.lower())
-        
+
         fh.write("}\n")
