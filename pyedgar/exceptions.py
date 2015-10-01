f9dcf3a6ba1fa917eb45cac0d8ec138315d247cf
Mac Gaulin <gaulinmp+git@gmail.com>
2015-10-01 12:17:41 -0500

    Replace FileNotFound with stdlib FileNotFoundError

--- pyedgar/exceptions.py
+++ pyedgar/exceptions.py
@@ -10,20 +10,14 @@ This module contains pyEDGAR's exceptions.
 class InputTypeError(Exception):
     """Input to method is of wrong type."""
 
-
 class WrongFormType(Exception):
     """Form provided is of wrong type"""
 
-
 class NoFormTypeFound(Exception):
     """Form type not found in file (no <FORM-TYPE> tag)"""
-
 
 class NoCIKFound(Exception):
     """CIK code not found in file (no <CIK> tag)"""
 
 class EDGARFilingFormatError(Exception):
     """File does not appear to be in proper EDGAR SGML format."""
- 
-class FileNotFound(Exception):
-    """File not found at path"""
